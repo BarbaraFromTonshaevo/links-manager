d695d93df05226f35e0077e422fa7a699ab8f3d6
Varvara <elkinavarvara1999@gmail.com>
2025-11-29 10:02:40 +0300

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Button from "primevue/button"
+</script>
+
+<template>
+    <main>
+        Links Manager!
+        <Button label="Submit" severity="warn" />
+    </main>
+</template>
