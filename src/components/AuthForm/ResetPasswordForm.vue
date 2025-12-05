4042d101b40967558b0935116a238a18814dc42a
Elkina Barbara <elkinavarvara1999@gmail.com>
2025-12-05 23:13:03 +0300

    feat [ResetPasswordForm]: layout

---
+++ src/components/AuthForm/ResetPasswordForm.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+
+const formData = ref({
+    email: '',
+})
+</script>
+
+<template>
+    <form>
+        <div class="mb-3">
+            <InputText name="email" placeholder="Введите email" type="text" v-model="formData.email" class="w-full" />
+        </div>
+        <div class="grid">
+            <Button type="submit" class="w-full" label="Сброс пароля"></Button>
+        </div>
+
+    </form>
+</template>
