2a0aebc9b66d0759f9ecb70b190410adcad8d49a
Elkina Barbara <elkinavarvara1999@gmail.com>
2025-12-05 23:17:05 +0300

    fix [main.js]: add primevue icon

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import App from './App.vue'
 import router from './router'
+import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 
