a1db95dc5048e610bf2095e08a29b09bc2f49a63
Elkina Barbara <elkinavarvara1999@gmail.com>
2025-12-05 23:12:21 +0300

    feat [Registrationform]: layout

---
+++ src/components/AuthForm/RegistrationForm.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+
+const formData = ref({
+    email: '',
+    password: '',
+    firstname: '',
+})
+</script>
+
+<template>
+    <form>
+        <div class="mb-3">
+            <InputText name="email" placeholder="Введите email" type="text" v-model="formData.email" class="w-full" />
+        </div>
+        <div class="mb-3">
+            <InputText name="password" placeholder="Введите пароль" type="password" v-model="formData.password" class="w-full" />
+        </div>
+        <div class="mb-3">
+            <InputText name="firstname" placeholder="Введите имя" type="text" v-model="formData.firstname" class="w-full" />
+        </div>
+        <div class="grid grid-cols-2 gap-3">
+            <Button type="submit" class="w-full" label="Регистрация"></Button>
+            <Button type="submit" icon="pi pi-github" class="w-full" label="GitHub" severity="contrast"></Button>
+        </div>
+    </form>
+</template>
